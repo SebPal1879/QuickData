90c3e516cf73fb8b28db6576e356d3e379400b80
Sebastián <spalacioalzate@gmail.com>
2024-06-10 22:43:45 -0500

    Mejora visual

--- QuickData/csv_xlsx/forms.py
+++ QuickData/csv_xlsx/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import archivo
 
 class archivo_formulario(forms.ModelForm):
-    nombre = forms.CharField(required=True,widget=forms.TextInput(attrs={'class': 'form-control form-control-lg','placeholder':'Nombre','required':''}))
+    nombre = forms.CharField(required=True,widget=forms.TextInput(attrs={'class': 'form-control form-control-lg','required':''}))
     subir_archivo = forms.FileField(required=True,widget=forms.FileInput(attrs={'class': 'form-control form-control-lg','type':'file','id':'formFile','required':''}))
     class Meta:
         model = archivo
